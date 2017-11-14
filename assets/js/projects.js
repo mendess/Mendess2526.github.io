0b6a1924c9c077e54fe71306047194098459d86c
Mendess2526 <pedro.mendes.26@gmail.com>
2017-11-14 10:37:57 +0000

    Creates Project categories and now includes all public repos

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,31 +1,42 @@
 window.onload = loadProjects();
 
 
-function projectHtml (name,description) {
+function projectHtml (name,description,link) {
 	var html = "";
 	html+="\t<li class=\"post-item\">";
 	html+="\t\t<h2 class=\"post-header\">";
-    html+="\t\t\t<a class=\"post-link\" href=\"/"+name+"\">"+name+"<i class=\"fa fa-long-arrow-right\" aria-hidden=\"true\"></i></a>";
-    html+="\t\t</h2>"
-    html+="\t\t<p class=\"post-description\">"+description+"</p>"
-    html+="\t</li>"
+	html+="\t\t\t<a class=\"post-link\" href=\""+link+"\">"+name+" <i class=\"fa fa-long-arrow-right\" aria-hidden=\"true\"></i></a>";
+	html+="\t\t</h2>"
+	if(description!=null){
+		html+="\t\t<p class=\"post-description\">"+description+"</p>"
+	}
+	html+="\t</li>"
 
-    return html;
+	return html;
 }
 
-function printProject (project) {
-	document.getElementById('post-list').innerHTML += projectHtml(project.name,project.description);
+function printProject (project,element,has_pages) {
+	var link;
+	if(has_pages){
+		link = "/"+project.name;
+	}else{
+		link = "https://github.com/Mendess2526/"+project.name;
+	}
+	if(project.name!="Mendess2526.github.io"){
+		document.getElementById(element).innerHTML += projectHtml(project.name,project.description,link);  
+	}
 }
 
 function printProjects () {
 	console.log("tou no printProjects");
 	var responseObj = JSON.parse(this.responseText);
-    //console.log(responseObj.name + " has " + responseObj.public_repos + " public repositories!");
-    for (var i = 0; i < responseObj.length; i++) {
-    	if(responseObj[i].has_pages && responseObj[i].name!="Mendess2526.github.io"){
-    		printProject(responseObj[i]);
-    	}
-    }
+	for (var i = 0; i < responseObj.length; i++) {
+		if(responseObj[i].has_pages){
+			printProject(responseObj[i],"main-project-list",responseObj[i].has_pages);
+		}else{
+			printProject(responseObj[i],"secondary-project-list",responseObj[i].has_pages);
+		}
+	}
 }
 
 function loadProjects () {
