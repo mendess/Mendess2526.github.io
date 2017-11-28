fd6484049e5365885dad7e7f807e60f33066fec5
Mendess2526 <pedro.mendes.26@gmail.com>
2017-11-28 16:05:17 +0000

    cleaned some code

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,4 +1,4 @@
-var objectsData = [];
+let objectsData = [];
 
 window.onload = loadStuff();
 
@@ -7,7 +7,7 @@ function loadStuff () {
 	activateButtons();
 }
 function loadProjects (mode) {
-	var request = new XMLHttpRequest();
+	let request = new XMLHttpRequest();
 	request.open('get','https://api.github.com/users/Mendess2526/repos', true);
 	request.send();
 	request.onreadystatechange = function(){
@@ -18,16 +18,16 @@ function loadProjects (mode) {
 	}
 }
 function listProjects (responseText,mode) {
-	var responseObj = JSON.parse(responseText);
+	let responseObj = JSON.parse(responseText);
 	if(objectsData.length==0){
-		for (var i = 0; i < responseObj.length; i++) {
-				var url;
+		for (let i = 0; i < responseObj.length; i++) {
+				let url;
 				if(responseObj.has_pages){
 					url = "/"+responseObj[i].name;
 				}else{
 					url = responseObj[i].html_url;
 				}
-				var tmp = {
+				let tmp = {
 					name: responseObj[i].name,
 					isFork: responseObj[i].fork,
 					stars: responseObj[i].stargazers_count,
@@ -36,7 +36,7 @@ function listProjects (responseText,mode) {
 					has_pages: responseObj[i].has_pages,
 					numCommits: -1,
 					lastCmtDate: "",
-				};
+				}
 				if(!tmp.isFork){
 					objectsData.push(tmp);
 				}
@@ -55,51 +55,51 @@ function listProjects (responseText,mode) {
 	/*}else{
 		printProjects();
 	}*/
-	for (var i = 0; i < objectsData.length; i++) {
+	for (let i = 0; i < objectsData.length; i++) {
 		setNumCommits(objectsData[i].name,i);
 		setLastCommitDate(objectsData[i].name,i);
-	};
+	}
 }
 function setNumCommits(name,repo) {
-	var request = new XMLHttpRequest();
+	let request = new XMLHttpRequest();
 	request.open('get','https://api.github.com/repos/Mendess2526/'+name+'/contributors?anon=true');
 	request.send();
 	request.onreadystatechange = function(){
 		if (request.readyState === 4 && (request.status === 200 || request.status === 304)){
 			if(name!="Mendess2526.github.io"){
-				var n = countCommits(JSON.parse(request.responseText));
+				let n = countCommits(JSON.parse(request.responseText));
 				objectsData[repo].numCommits = n;
 				document.getElementById('commits'+name).innerHTML = " #of commits: "+n;
-			} 
+			}
 		}
 		else console.log(request.status, objectsData);
 	}
 }
 function setLastCommitDate (name,repo) {
-	var request = new XMLHttpRequest();
+	let request = new XMLHttpRequest();
 	request.open('get','https://api.github.com/repos/Mendess2526/'+name+'/commits');
 	request.send();
 	request.onreadystatechange = function(){
 		if (request.readyState === 4 && (request.status === 200 || request.status === 304)){
-			var respTxt = JSON.parse(request.responseText);
-			var n = respTxt[0].commit.author.date;
+			let respTxt = JSON.parse(request.responseText);
+			let n = respTxt[0].commit.author.date;
 			objectsData[repo].lastCmtDate = n;
 		}
 		else console.log(request.status, objectsData);
 	}
 }
 /*function printProjects () {
-	var projBoxDiv = document.getElementById('project-box');
+	let projBoxDiv = document.getElementById('project-box');
 	projBoxDiv.innerHTML = "";
 
-	var div = document.createElement('div');
-	var ul = document.createElement('ul');
+	let div = document.createElement('div');
+	let ul = document.createElement('ul');
 	div.classList.add('post-box');
 	ul.classList.add('post-list');
 
-	for(var i = 0; i < objectsData.length; i++){
+	for(let i = 0; i < objectsData.length; i++){
 		if(objectsData[i].name!="Mendess2526.github.io"){
-			var li = buildRepoHtml(objectsData[i]);
+			let li = buildRepoHtml(objectsData[i]);
 			ul.appendChild(li);
 		}
 	}
@@ -107,31 +107,31 @@ function setLastCommitDate (name,repo) {
 	projBoxDiv.appendChild(div);
 }*/
 function printProjects_split () {
-	var projBoxDiv = document.getElementById('project-box');
+	let projBoxDiv = document.getElementById('project-box');
 	projBoxDiv.innerHTML = "";
-	var divMain = document.createElement('div');
-	var mainH2 = document.createElement('h2');
-	var ulMain = document.createElement('ul');
+	let divMain = document.createElement('div');
+	let mainH2 = document.createElement('h2');
+	let ulMain = document.createElement('ul');
 	divMain.classList.add('post-box');
 	ulMain.classList.add('post-list');
 	mainH2.innerHTML = "Main Projects";
 	divMain.appendChild(ulMain);
 	projBoxDiv.appendChild(mainH2);
 	projBoxDiv.appendChild(divMain);
-	
 
-	var divSecond = document.createElement('div');
-	var secondH2 = document.createElement('h2');
-	var ulSecond = document.createElement('ul');
+
+	let divSecond = document.createElement('div');
+	let secondH2 = document.createElement('h2');
+	let ulSecond = document.createElement('ul');
 	divSecond.classList.add('post-box');
 	secondH2.innerHTML = "Secondary Projects";
 	ulSecond.classList.add('post-list');
 	divSecond.appendChild(ulSecond);
 	projBoxDiv.appendChild(secondH2);
 	projBoxDiv.appendChild(divSecond);
-	for(var i = 0; i < objectsData.length; i++){
+	for(let i = 0; i < objectsData.length; i++){
 		if(objectsData[i].name!="Mendess2526.github.io"){
-			var li = buildRepoHtml(objectsData[i]);
+			let li = buildRepoHtml(objectsData[i]);
 			if(objectsData[i].has_pages){
 				ulMain.appendChild(li);
 			}else{
@@ -141,10 +141,10 @@ function printProjects_split () {
 	}
 }
 function buildRepoHtml(repo) {
-	var li = document.createElement('li');
-	var headerH2  = buildHeader(repo);
-	var descP 	  = buildDescription(repo);
-	var footerDiv = buildFooter(repo);
+	let li = document.createElement('li');
+	let headerH2  = buildHeader(repo);
+	let descP 	  = buildDescription(repo);
+	let footerDiv = buildFooter(repo);
 	li.classList.add('post-item');
 	li.appendChild(headerH2);
 	li.appendChild(descP);
@@ -154,9 +154,9 @@ function buildRepoHtml(repo) {
 }
 function buildHeader (repo) {
 	//create elements
-	var h2 = document.createElement('h2');
-	var a = document.createElement('a');
-	var i = document.createElement('i');
+	let h2 = document.createElement('h2');
+	let a = document.createElement('a');
+	let i = document.createElement('i');
 	//add CSS classes and oher atributes
 	h2.classList.add('post-header');
 	a.classList.add('post-link');
@@ -167,24 +167,24 @@ function buildHeader (repo) {
 	a.innerHTML = repo.name + " ";
 	a.appendChild(i);
 	h2.appendChild(a);
-	
+
 	return h2;
 }
 function buildDescription (repo) {
 	//create elements
-	var p = document.createElement('p');
+	let p = document.createElement('p');
 	//add CSS classes and oher atributes
 	p.classList.add('post-description');
 	//add content
 	p.innerHTML = repo.description;
-	return p;		
+	return p;
 }
 function buildFooter (repo) {
 	//create elements
-	var div = document.createElement('div');
-	var span1 = document.createElement('span');
-	var span2 = document.createElement('span');
-	var span3 = document.createElement('span');
+	let div = document.createElement('div');
+	let span1 = document.createElement('span');
+	let span2 = document.createElement('span');
+	let span3 = document.createElement('span');
 	//add CSS classes and oher atributes
 	div.classList.add('article-list-footer');
 	span1.className = 'article-list-date divider';
@@ -205,15 +205,15 @@ function buildFooter (repo) {
 	return div;
 }
 function countCommits (response) {
-	var count = 0;
-	for (var i = 0; i < response.length; i++) {
+	let count = 0;
+	for (let i = 0; i < response.length; i++) {
 		count += response[i].contributions;
-	};
+	}
 	return count;
 }
 function dateCompare (a,b) {
-	var dateA = new Date(a.lastCmtDate);
-	var dateB = new Date(b.lastCmtDate);
+	let dateA = new Date(a.lastCmtDate);
+	let dateB = new Date(b.lastCmtDate);
 	return dateB -  dateA;
 }
 function starCompare(a,b){
@@ -236,4 +236,4 @@ function activateButtons(){
 	document.getElementById('bStars').setAttribute('onclick','sortByStars()');
 	document.getElementById('bCommits').setAttribute('onclick','sortByCommits()');
 	document.getElementById('bDefault').setAttribute('onclick','defaultSort()');
-}	
+}
